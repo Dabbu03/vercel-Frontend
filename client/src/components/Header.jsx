c526e5a5eb31a120832a6411bc0e85b03b85cd8f
Dipin003 <kharayatdipin@gmail.com>
2025-04-10 00:22:40 +0530

    Add Animation in HomePage with the help of GSAP

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,25 +1,68 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap'
 
 const Header = () => {
 
+    useGSAP(() => {
+        // const tl = gsap.timeline();
+        const tl = gsap.timeline({ delay: 1.5 });
+
+        tl.from('.head1', {
+            y: 100,
+            opacity: 0,
+            duration: 1.5,
+            scale: 2,
+            ease: 'bounce',
+            stagger: 0.5
+        })
+            tl.from('.head2', {
+                x: 100,
+                opacity: 0,
+                duration: 1.5,
+                scale: 1.2,
+                ease: 'bounce.out'
+            }, "-=1") 
+            tl.from('.txt', {
+                x: -100,
+                opacity: 0,
+                duration: 1.5
+            }, "-=1")
+           tl.from('.btn', {
+                y: 100,
+                opacity: 0,
+                duration: 1.5,
+                scale: 1.1,
+                ease: 'expo.inOut'
+            }, '-=1')
+            tl.from('.img', {
+                y: 100,
+                opacity: 0,
+                duration: 2,
+                ease: 'elastic.out(1, 0.3)'
+            }, '-=1.2');
+    });
+
+
     return (
         <div className='flex flex-col justify-center items-center text-center my-20'>
-            <div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500'>
-                <p>Best text to image generator</p>
+
+            <div className='head1 text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500'>
+                <p >Best text to image generator</p>
                 <img src={assets.star_icon} alt="" />
 
             </div>
 
-            <h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'>Turn Text to <span className='text-blue-600'>image</span> , in seconds.</h1>
+            <h1 className='head2 text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'>Turn Text to <span className='text-blue-600'>image</span> , in seconds.</h1>
 
-            <p className='text-center max-w-xl mx-auto mt-5'>Unleash your creativity with AI. Turn your imagination into visual art in seconds - just type, and watch the magic happen.</p>
+            <p className='head2 text-center max-w-xl mx-auto mt-5'>Unleash your creativity with AI. Turn your imagination into visual art in seconds - just type, and watch the magic happen.</p>
 
-            <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'> Generate Images
+            <button className='btn sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'> Generate Images
                 <img className='h-6' src={assets.star_group} alt="start_groupImages" />
             </button>
 
-            <div className='flex flex-wrap justify-center mt-16 gap-3'>
+            <div className='img flex flex-wrap justify-center mt-16 gap-3'>
                 {Array(6).fill('').map((item, index) => (
                     <img className='rounded hover:scale-1.5 transition-all duration-300 cursor-pointer max-sm:w-10'
                         src={index % 2 === 0 ? assets.sample_img_2 : assets.sample_img_1}
@@ -28,7 +71,7 @@ const Header = () => {
 
             </div>
 
-            <p className='mt-2 text-neutral-600'>Generate images from imagify</p>
+            <p className='txt mt-2 text-neutral-600'>Generate images from imagify</p>
         </div>
     )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,16 +2,41 @@ import { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { Link, useNavigate } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
-
+import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap'
 
 const Navbar = () => {
+    const tl = gsap.timeline()
+
+    // Animation using GSAP
+    useGSAP(() => {
+        tl.from('.logo', {
+            x: -150,
+            opacity: 0,
+            duration: 1.2,
+            ease: 'power4.out',
+            rotate: -15,
+            scale: 0.8
+          });
+        
+          tl.from('.nav', {
+            y: -100,
+            opacity: 0,
+            rotate: 200,
+            duration: 1.2,
+            stagger: 0.15,
+            ease: 'back.out(1.7)', 
+            scale: 0.95
+          }, "-=0.8"); 
+
+    })
 
     const { user } = useContext(AppContext)
 
     const navigate = useNavigate()
 
     return (
-        <div className='flex items-center justify-between py-4'>
+        <div className='logo flex items-center justify-between py-4'>
             <Link to='/'>
                 <img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40' />
             </Link>
@@ -19,17 +44,17 @@ const Navbar = () => {
             <div>
                 {
                     user ?
-                        <div className='flex items-center gap-2 sm:gap-3'>
+                        <div className='nav flex items-center gap-2 sm:gap-3'>
                             <button
                                 onClick={() => navigate('/buy')}
                                 className='flex items-center gap-2 bg-blue-200 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700'>
                                 <img className='w-5' src={assets.credit_star} alt="" />
                                 <p className='text-xs sm:text-sm font-medium text-gray-600'>Credits left: 50</p>
                             </button>
-                            <p className='text-gray-600 max-sm:hidden pl-4'>Hi, Dabbu</p>
+                            <p className='nav text-gray-600 max-sm:hidden pl-4'>Hi, Dabbu</p>
 
-                            <div className='relative group'>
-                                <img className='w-10 drop-shadow' src={assets.profile_icon} alt="" />
+                            <div className='nav relative group'>
+                                <img className='nav w-10 drop-shadow' src={assets.profile_icon} alt="" />
                                 <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
                                     <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
                                         <li className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
@@ -38,11 +63,11 @@ const Navbar = () => {
                             </div>
                         </div>
                         :
-                        <div className='flex items-center gap-2 sm:gap-5'>
+                        <div className='nav flex items-center gap-2 sm:gap-5'>
                             <p
                                 onClick={() => navigate('/buy')}
-                                className='cursor-pointer'>Pricing</p>
-                            <button className='bg-zinc-900 text-white px-6 py-2 sm:px-10 text-sm rounded-full'>Login</button>
+                                className='nav cursor-pointer'>Pricing</p>
+                            <button className='nav bg-zinc-900 text-white px-6 py-2 sm:px-10 text-sm rounded-full'>Login</button>
                         </div>
                 }
             </div>
