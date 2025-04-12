fe519a491de1774537197d291e92561e14333c90
Dipin003 <kharayatdipin@gmail.com>
2025-04-12 10:50:12 +0530

    Result and Buy credit Pages UI added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import {  assets } from '../assets/assets'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
     return (
         <div className='flex items-center justify-between gap-4 py-3 mt-20'>
             <img src={assets.logo} width={150} alt="" />
 
-            <p className='flex-1 border-1 border-gray-500 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @DipinKharayat.dev | All right reserved.</p>
+            <p className='flex-1 border-l border-gray-500 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @DipinKharayat.dev | All right reserved.</p>
 
             <div className='flex gap-2.5'>
                 <img src={assets.facebook_icon} width={35} alt="" />
