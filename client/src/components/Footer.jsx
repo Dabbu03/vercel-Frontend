de44194e129623f5e35bcc0e990ec16d667adf55
Dipin003 <kharayatdipin@gmail.com>
2025-07-20 11:12:40 +0530

    Fixing Bug

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,22 +2,29 @@ import { assets } from '../assets/assets'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
-    return (
-        <div className='flex items-center justify-between gap-4 py-3 mt-20'>
-            <Link to='/'>
-                <h1 className='text-2xl font-extralight sm:text-3xl md:text-4xl lg:text-5xl cursor-pointer hover:scale-105 transition-transform duration-300 '>
-                    Pixora
-                </h1>
-            </Link>
+  return (
+    <div className='flex items-center justify-between gap-4 py-3 mt-20'>
+      <Link to='/'>
+        <h1 className='text-2xl font-extralight sm:text-3xl md:text-4xl lg:text-5xl cursor-pointer hover:scale-105 transition-transform duration-300 '>
+          Pixora
+        </h1>
+      </Link>
 
-            <p className='flex-1 border-l border-gray-500 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @DipinKharayat.dev | All right reserved.</p>
+      <p className='flex-1 border-l border-gray-500 pl-4 text-sm text-gray-500 max-sm:hidden'>
+        © DipinKharayat.dev | All rights reserved.
+      </p>
 
-            <div className='flex gap-2.5'>
-                <a href="https://instagram.com/dipinkharayat_03" target='_blank'><img src={assets.instagram_icon} width={35} alt="" />
-                </a>
-            </div>
-        </div>
-    )
+      <div className='flex gap-2.5'>
+        <a
+          href='https://instagram.com/dipinkharayat_03'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img src={assets.instagram_icon} width={35} alt='Instagram' />
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
